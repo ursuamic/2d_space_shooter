1a63b5d2489df6a70b6ecefcf836f351fe676001
Bear Friend <bjornwann@yahoo.com>
2020-10-30 01:10:49 +0100

    git init

---
+++ Assets/SpaceShip.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpaceShip : MonoBehaviour
+{   
+    
+    Rigidbody2D rb;
+    public float speed;
+
+    void Awake() {
+        rb = GetComponent<Rigidbody2D>();
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rb.AddForce(new Vector2(Input.GetAxis("Horizontal") * speed, 0));
+    }
+}
