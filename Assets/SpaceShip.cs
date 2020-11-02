62c15db259619b3a9351ab5dd9b7d8d097cb9d45
ursuamic <bjorn.wann@gmail.com>
2020-11-02 02:26:02 +0100

    add enemy and collision

--- Assets/SpaceShip.cs
+++ Assets/SpaceShip.cs
@@ -3,23 +3,35 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class SpaceShip : MonoBehaviour
-{   
-    
-    Rigidbody2D rb;
-    public float speed;
+{
 
-    void Awake() {
+    Rigidbody2D rb;
+    public float speed = 5;
+    public float health = 1;
+
+    void Awake()
+    {
         rb = GetComponent<Rigidbody2D>();
     }
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
         rb.AddForce(new Vector2(Input.GetAxis("Horizontal") * speed, 0));
+        rb.AddForce(new Vector2(0, Input.GetAxis("Vertical") * speed));
+    }
+
+    public void Damage()
+    {
+        health--;
+        if(health==0)
+        {
+            Destroy(gameObject);
+        }
     }
 }
---
+++ Assets/fastship.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class fastship : MonoBehaviour
+{
+    Rigidbody2D rb;
+
+    public float xSpeed;
+    public float ySpeed;
+    public bool canShoot;
+    public float fireRate;
+    public float health;
+    // Start is called before the first frame update
+
+    void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+    
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rb.velocity = new Vector2(xSpeed, ySpeed * -1);
+    }
+
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        Debug.Log(col.gameObject.tag);
+        if (col.gameObject.tag == "Player")
+        {
+            col.gameObject.GetComponent<SpaceShip>().Damage();
+            Die();
+        }
+
+        void Die()
+        {
+            Destroy(gameObject);
+        }
+    }
+}
+
+
